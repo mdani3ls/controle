082dfc6dabce823296b5d7e12614fc8247a4926b
mdani3ls <dsm0ra3s@gmail.com>
2024-07-09 23:31:20 -0300

    pwa

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('app-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/script.js',
+          '/manifest.json',
+          // Adicione aqui todos os arquivos que precisam ser cacheados
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
