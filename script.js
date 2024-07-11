649fb0bb71ae2cfa61c702f47932aa4ace420464
mdani3ls <dsm0ra3s@gmail.com>
2024-07-11 20:07:03 -0300

    pwa

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registrado com sucesso:', registration);
-      })
-      .catch((error) => {
-        console.log('Falha ao registrar o Service Worker:', error);
-      });
-  }
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/service-worker.js')
+//       .then((registration) => {
+//         console.log('Service Worker registrado com sucesso:', registration);
+//       })
+//       .catch((error) => {
+//         console.log('Falha ao registrar o Service Worker:', error);
+//       });
+//   }
   
 document.addEventListener("DOMContentLoaded", function() {
     // Criando o objeto com as propriedades especificadas
--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,26 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('app-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/script.js',
-          '/manifest.json',
-          '/fonts'
-          // Adicione aqui todos os arquivos que precisam ser cacheados
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+var cacheName = 'cache';
+var filesToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
